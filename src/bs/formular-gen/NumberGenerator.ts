f8831dd5cd16ed83e075aed20ebc4311e2233347
moemoefish <moemoefish@qq.com>
2023-04-10 20:37:28 +0800

    daily

---
+++ src/bs/formular-gen/NumberGenerator.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: moemoefish moemoefish@qq.com
+ * @Date: 2023-04-10 20:31:34
+ * @LastEditors: moemoefish moemoefish@qq.com
+ * @LastEditTime: 2023-04-10 20:37:05
+ * @Description: 符号产生器
+ */
+const obelus = '÷';
