9c66d626760d882e3504818bdba36f0566ef5774
moemoefish <moemoefish@qq.com>
2023-04-17 19:28:21 +0800

    feat: add GroupGenerator

--- src/bs/formula-gen/GroupGenerator.ts
+++ src/bs/formula-gen/GroupGenerator.ts
@@ -4,8 +4,8 @@ import type { RuleGroup } from "../arith-rules/RuleGroup";
  * @Author: moemoefish moemoefish@qq.com
  * @Date: 2023-04-14 10:17:55
  * @LastEditors: moemoefish moemoefish@qq.com
- * @LastEditTime: 2023-04-14 10:35:41
- * @Description: 
+ * @LastEditTime: 2023-04-17 19:27:47
+ * @Description: generator for group rule
  */
 export interface GroupGenerator {
     generate(): Node | null
@@ -21,6 +21,8 @@ export function createGroupGenerator(rule: RuleGroup) {
             if (showType == "hide") {
                 return null
             }
+
+            
 
             return null
         }
