5ab3db683b01fc871eccc0db1b0a224228425050
moemoefish <moemoefish@qq.com>
2023-04-27 20:40:26 +0800

    feat: change config

--- src/utils/randomUtils.ts
+++ src/utils/randomUtils.ts
@@ -2,7 +2,7 @@
  * @Author: moemoefish moemoefish@qq.com
  * @Date: 2023-04-25 11:36:30
  * @LastEditors: moemoefish moemoefish@qq.com
- * @LastEditTime: 2023-04-25 20:27:31
+ * @LastEditTime: 2023-04-27 20:29:43
  * @Description: 
  */
 
@@ -10,15 +10,24 @@
 
 /**
  * 产生 >= min，<=max 的随机整数
- * @param {any} min:number
- * @param {any} max:number
+ * @param {number} min - 整数
+ * @param {number} max - 整数
  * @returns {number} 
  */
 export function createRandomRange(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + Math.floor(min)
 }
 
-export function getRandomArrayElement<T>(arr: T[]): T {
+/**
+ * 随机从数组中的获取一项
+ * @param {T} arr 
+ * @returns 
+ */
+export function getRandomArrayElement<T>(arr: T[]): T | null {
+    if (!arr) {
+        return null
+    }
+
     const index = createRandomRange(0, arr.length - 1)
 
     return arr[index]
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: moemoefish moemoefish@qq.com
+ * @Date: 2023-04-10 19:09:17
+ * @LastEditors: moemoefish moemoefish@qq.com
+ * @LastEditTime: 2023-04-27 19:28:23
+ * @Description: 
+ */
 import { fileURLToPath } from 'node:url'
 import { mergeConfig } from 'vite'
 import { configDefaults, defineConfig } from 'vitest/config'
@@ -9,7 +16,12 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src'))
+      }
     }
   })
 )
